0ad56e63d9561f6778204d81b82fa3ff55d7de48
TakuyaIto <zurausa77@gmail.com>
2019-05-16 16:45:41 -0700

    fix or problem

--- iroha_1/12.py
+++ iroha_1/12.py
@@ -1,47 +1,96 @@
+''' sparse table
+参考コード(https://atcoder.jp/contests/iroha2019-day1/submissions/5340178 )
+n,k = 9 37
+a=[2,0,1,2,5,7,0,2,3]のときについて解説
+
+sparse_table[n][n.bit_length()] で構築
+※以降stと表記
+st[0] には 長さ2**0のa[i]のみを代入
+st[1] には 長さ2**1のa[i:i+1]のor値を入力
+(ex: st[1] = [2,1,3,7,7,7,2,3,0])となる ※右詰で
+st[2] には 長さ2**2のa[i:i+3]の全部のor値を入力
+これはst[1]を利用すれば最小限のコストで構築可能
+同様に処理
+[2,0,1,2,5,7,0,2,3],
+[2,1,2,7,7,7,2,3,0],
+[2,7,7,7,7,7,0,0,0],
+[7,7,0,0,0,0,0,0,0]
+
+・検索部分について
+利用するst列に関しては
+検索の長さから決定する
+ex:) r-l = 0,1 => 0列目
+         = 2,3 => 1列目
+         = 4,5,6,7 => 2列目
+         ...
+これはログテーブルを作成すれば問題なし
+# log_table = [0,0,1,1,2,2,2,2,3,3,3,3,3....]
+for i in range(2, len(self.log_table)):
+            self.log_table[i] = self.log_table[i//2] + 1
+
+or部分に関しては右側をr - (1 << row)のようにする
+同じ要素をorしても解は変わらないので
+例えば(L,R) = (0,3)のような場合
+L ... st[1][0] => (a[0] | a[1] )
+R ... st[1][1] => (a[1] | a[2] )
+になる。
+
+L,RのOR結果が二分探索で出している値より大きい場合、
+それ以降のRに関しては同様の結果が求められるため、
+cnt += n - r + 1とできる
+これを繰り返し、cnt がkより大きければ探索値を切り替える
+ '''
+
 import math
 import numpy as np
 n, k = map(int, input().split())
 a = [int(input()) for _ in range(n)]
-bit_range = math.floor(math.log2(max(a)))+1
-mp = np.reshape(np.zeros(bit_range*(n+1)), (n+1, bit_range))
-for i in range(n):
-    tmp = np.array(list(map(int, list(format(a[i], 'b')))))
-    if tmp.size < bit_range:
-        tmp = np.pad(tmp, (bit_range-tmp.size, 0), 'constant')
-    mp[i + 1] = mp[i] + tmp
-mid = 2 ** (bit_range-1)
-mx = 2 ** bit_range - 1
-mn = 0
+
+lt = [0 for _ in range(n+1)]
+for i in range(2, n + 1):
+    lt[i] = lt[i//2]+1
 
 
-def mp_to_int(x):
-    ret = 0
-    l = len(x)
-    for i, x in enumerate(x.tolist()):
-        if x > 0:
-            ret += 2 ** (l - i - 1)
-    return ret
+def make_st(a, n):
+    rs = n.bit_length()
+    st = [[0]*n for _ in range(rs)]
+    for i in range(n):
+        st[0][i] = a[i]
+    for row in range(1, rs):
+        for i in range(n - (1 << row) + 1):
+            st[row][i] = st[row - 1][i] | st[row - 1][i + (1 << (row - 1))]
+    return st
 
 
-ans = []
-while True:
+def query(l, r):
+    if l == r:
+        return - 1
+    row = lt[r - l]
+    return st[row][l] | st[row][r - (1 << row)]
+
+
+def solve(mid):
+    l = 0
+    r = 1
     cnt = 0
-    for i in range(1, n+1):
-        if a[i - 1] >= mid:
-            cnt += n+1 - i
-        else:
-            for j in range(i+1, n + 1):
-                if mp_to_int(mp[j] - mp[i - 1]) >= mid:
-                    cnt += n + 1 - j
-                    break
+    while True:
+        while (r < n and query(l, r) < mid):
+            r += 1
+        if query(l, r) < mid:
+            break
+        cnt += n - r + 1
+        l += 1
+    return cnt
 
-    if cnt >= k:
-        ans.append(mid)
-        mn = mid
-        mid = math.ceil((mn + mx)/2)
-    else:
+
+st = make_st(a, n)
+mn = 0
+mx = query(0, n) + 1
+
+while abs(mx - mn) > 1:
+    mid = (mn + mx) // 2
+    if solve(mid) < k:
         mx = mid
-        mid = math.floor((mn + mx) / 2)
-    if mid in ans:
-        break
-print(mid)
+    else:
+        mn = mid
+print(mn)
